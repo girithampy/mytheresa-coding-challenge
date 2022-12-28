6802a82967d597ea72e2d77dda2aa4daedbde014
girithampy <giriprasadthampy@gmail.com>
2022-12-28 15:24:56 +0100

    Webpack asset modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Header from "./components/Header";
+import image from './images/user_placeholder.png'
+
 import "./App.scss";
 
-const App = () => <h1 className="title">Hello from React! 123</h1>;
-
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <img src={image}/>
+            <h1 className="title">Hello from React! 123</h1>
+        </div>
+    );
+}
 export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,5 @@
+import "./style.scss";
+
+const Header = () => <h1 className="header-title">Header</h1>;
+
+export default Header;
